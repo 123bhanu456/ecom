b9e3d58f579f54bc88d7453ef62255e2d884dc52
123bhanu456 <kalahastribahnuprakash@gmail.com>
2023-11-06 19:48:54 +0530

    login and signup forms styling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -31,43 +31,41 @@ function Login(){
     }
     return(
         
-       <div className='container'>
-        <div className='condiv'>
-        <h3>Login</h3>
-        <form onSubmit={handlesub}>
-
-            <div className='indiv'>
-            <label className='lab'>email</label>
-            <input 
-            type="email" 
-            className='inp'
-            placeholder=' Enter email'
-            onChange={(e)=>setemail(e.target.value)}/>
+     
+      <div class="container">
+      <div class="screen">
+        <div class="screen__content">
+          <form class="login" onSubmit={handlesub}>
+            <div class="login__field">
+              <i class="login__icon fas fa-user"></i>
+              <input type="text" class="login__input" placeholder="User name / Email"  onChange={(e)=>setemail(e.target.value)}/>
             </div>
-
-
-            <div className='indiv'>
-            <label className='lab'>Password</label>
-            <input 
-            type="password" 
-            className='inp'
-            placeholder='Enter Password'
-            onChange={(e)=>setpswd(e.target.value)}/>
+            <div class="login__field">
+              <i class="login__icon fas fa-lock"></i>
+              <input type="password" class="login__input" placeholder="Password"  onChange={(e)=>setpswd(e.target.value)}/>
             </div>
+            <button class="button login__submit">
+              <span class="button__text">Log In Now</span>
+              <i class="button__icon fas fa-chevron-right"></i>
+            </button>			
 
-            <div className='btdiv'>
-            <button className='bt' onClick={handlesub}>Login</button>
-          </div>
-
-            <div className='btdiv'>
-               <Link to="/SignUp" className='col'>SignUp</Link> 
-            </div>
             
-        </form>
-        </div>
-             
-       </div>
+            <Link to="/SignUp" className='sign'>SignUp</Link> 
+              
           
+
+              	
+          </form>
+          
+        </div>
+        <div class="screen__background">
+          <span class="screen__background__shape screen__background__shape4"></span>
+          <span class="screen__background__shape screen__background__shape3"></span>		
+          <span class="screen__background__shape screen__background__shape2"></span>
+          <span class="screen__background__shape screen__background__shape1"></span>
+        </div>		
+      </div>
+    </div>
       
     )
 }
--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -30,63 +30,51 @@ function Signup(){
 
     }
     return(
-        
-       <div className='container'>
-        <div className='condiv'>
-        <h3>SignUp </h3>
-        <form>
-            <div className='indiv'>
-            <label className='lab' >First name</label>
-            <input 
-            type="text" 
-            className='inp'
-            placeholder='First name'
-            onChange={(e)=>setfname(e.target.value)}/>
-            </div>
-
-
-            <div className='indiv'>
-            <label className='lab'>Last name</label>
-            <input 
-            type="text" 
-            className='inp'
-            placeholder='Last name'
-            onChange={(e)=>setlname(e.target.value)}/>
-            </div>
-
-
-            <div className='indiv'>
-            <label className='lab'>email</label>
-            <input 
-            type="email" 
-            className='inp'
-            placeholder=' Enter email'
-            onChange={(e)=>setemail(e.target.value)}/>
-            </div>
-
-
-            <div className='indiv'>
-            <label className='lab'>Password</label>
-            <input 
-            type="password" 
-            className='inp'
-            placeholder='Enter Password'
-            onChange={(e)=>setpswd(e.target.value)}/>
-            </div>
-
-            {/* <div className='indiv'>
-             <input type="submit" value="Signup" className='btn inp' />
-            </div> */}
-
-           <div className='btdiv'>
-            <button className='bt' onClick={handlesub}>SignUp</button>
-          </div>
-            
-        </form>
-        </div>
-             
-       </div>
+   
           
+      <div class="container">
+      <div class="screen">
+        <div class="screen__content">
+          <form class="signin" onSubmit={handlesub}>
+            <div class="signin__field">
+            <i class="signin__icon fas fa-user"></i>
+              <input type="text" class="signin__input" placeholder="First Name"  onChange={(e)=>setfname(e.target.value)}/>
+            </div>
+            <div class="signin__field">
+            <i class="signin__icon fas fa-user"></i>
+              <input type="text" class="signin__input" placeholder="Last Name"  onChange={(e)=>setlname(e.target.value)}/>
+            </div>
+            <div class="signin__field">
+              <i class="signin__icon fas fa-user"></i>
+              <input type="text" class="signin__input" placeholder="User name / Email"  onChange={(e)=>setemail(e.target.value)}/>
+            </div>
+            <div class="signin__field">
+              <i class="signin__icon fas fa-lock"></i>
+              <input type="password" class="signin__input" placeholder="Password"  onChange={(e)=>setpswd(e.target.value)}/>
+            </div>
+            <button class="button signin__submit">
+              <span class="button__text">SIGN IN</span>
+              <i class="button__icon fas fa-chevron-right"></i>
+            </button>			
+
+            
+           
+              
+          
+
+              	
+          </form>
+          
+        </div>
+        <div class="screen__background">
+          <span class="screen__background__shape screen__background__shape4"></span>
+          <span class="screen__background__shape screen__background__shape3"></span>		
+          <span class="screen__background__shape screen__background__shape2"></span>
+          <span class="screen__background__shape screen__background__shape1"></span>
+        </div>		
+      </div>
+    </div>
+      
       
     )
 }
