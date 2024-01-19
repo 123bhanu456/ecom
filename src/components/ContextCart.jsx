df34afaa16ea95f36e12f917de186c631b669515
123bhanu456 <kalahastribahnuprakash@gmail.com>
2024-01-19 09:56:54 +0530

    resolved errors

--- src/components/ContextCart.jsx
+++ src/components/ContextCart.jsx
@@ -52,7 +52,7 @@ function ContextCart(){
     <header>
   <div className='shop'>
   <div>
-  <NavLink to='./UserHome'> <i className="fa-solid fa-arrow-right-long"> Shop More </i></NavLink>
+  <NavLink to='/men'> <i className="fa-solid fa-arrow-right-long color"> Shop More </i></NavLink>
   </div>
   <div>
   <i class="fa-sharp fa-solid fa-cart-shopping">{totalItem}</i>
